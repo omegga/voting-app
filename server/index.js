69aba3a7077bd2092f35bb971420bf1f651ac6b9
omegga <whymagnet@gmail.com>
2019-09-24 16:54:46 +0200

    enable proxy for webpack-dev-server and add request logger on server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ dotenv.config();
 
 const app = express();
 
+function requestLogger(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+}
+app.use(requestLogger);
 app.use(express.static(path.join(__dirname, "../build")));
 
 const PORT = process.env.PORT;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,14 @@
 const merge = require('webpack-merge');
+const dotenv = require("dotenv");
+dotenv.config();
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
 	mode: 'development',
 	devServer: {
-		hot: true
+		hot: true,
+		proxy: {
+			'/api': 'http://localhost:' + process.env.PORT
+		}
 	}
 });
