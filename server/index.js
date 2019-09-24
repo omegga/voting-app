64e1951ff75ff44fa2bacbd9bd3e1b677fcbf456
omegga <whymagnet@gmail.com>
2019-09-24 16:28:28 +0200

    add git hooks and eslint for server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, "../build")));
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log('listening on port ' + PORT));
+app.listen(PORT, () => console.log("listening on port " + PORT));
