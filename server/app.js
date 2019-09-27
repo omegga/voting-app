27b62fcc1d3520531c252c0dd1a696e4b16aca68
omegga <whymagnet@gmail.com>
2019-09-27 05:13:01 +0200

    add error handlers

--- server/app.js
+++ server/app.js
@@ -5,12 +5,30 @@ const loginRoute = require("./login/loginRoute");
 const pollsRoute = require("./polls/pollsRoute");
 const authRoute = require("./auth/authRoute");
 
+function logErrors(error, req, res, next) {
+	console.error("error.name", error.name);
+	console.error("error.message", error.message);
+	console.error("error.stack", error.stack);
+	next(error);
+}
+
+function jwtErrorsHandler(error, req, res, next) {
+	if (error.name === "TokenExpiredError") {
+		return res.status(401).json({ error: "token expired" });
+	}
+	if (error.name === "JsonWebTokenError") {
+		return res.status(401).json({ error: "invalid token" });
+	}
+	next(error);
+}
+
+function requestLogger(req, res, next) {
+	console.log(req.method, req.url);
+	next();
+}
+
 function loadApp(app) {
 	app.use(express.json());
-	function requestLogger(req, res, next) {
-		console.log(req.method, req.url);
-		next();
-	}
 	app.use(requestLogger);
 	usersRoute(app);
 	loginRoute(app);
@@ -21,17 +39,8 @@ function loadApp(app) {
 	app.use("*", (req, res) => {
 		res.sendFile(indexHtmlFile);
 	});
-	app.use(function errorHandler(error, req, res, next) {
-		console.error("error.name", error.name);
-		console.error("error.message", error.message);
-		if (error.name === "TokenExpiredError") {
-			return res.status(401).json({ error: "token expired" });
-		}
-		if (error.name === "JsonWebTokenError") {
-			return res.status(401).json({ error: "invalid token" });
-		}
-		next(error);
-	});
+	app.use(logErrors);
+	app.use(jwtErrorsHandler);
 }
 
 module.exports = loadApp;
